502d20c0590e96bf5956b849e77775ab0c879a4a
Dave Norris <dockerdave00@gmail.com>
2021-09-12 17:03:01 -0600

    added dynamic config file for IP/port

--- prep_build_aws_instance.sh
+++ prep_build_aws_instance.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
-      HOST_IP="3.17.172.233"
-    HTTP_PORT="80"
-
       MY_HOME="/home/dnorris"
      EC2_HOME="/home/ec2-user"
+
+      HOST_IP=$(cat ${MY_HOME}/instance_ip.dat | awk '{print $1}')
+    HTTP_PORT=$(cat ${MY_HOME}/instance_ip.dat | awk '{print $2}')
+
    SOURCE_DIR="${MY_HOME}/homework/hello-python"
 DB_SCRIPT_DIR="${MY_HOME}/tools"
      PEM_FILE="${MY_HOME}/dockerdave00_aws_instance_keypair.pem"
@@ -21,19 +22,19 @@ for FILE in app.py \
 	    postgres_setup.sh
 	do
 		if [ ${FILE} != "postgres_setup.sh" ]; then
-			echo -e "\tCopying ${FILE}"
+			echo -e "\tcopy: ${FILE}"
 			 scp -i ${PEM_FILE} ${SOURCE_DIR}/${FILE} ec2-user@${HOST_IP}:./ 2<&1 > /dev/null
 	
 			if [ ${FILE} == "app.py" ]; then
-				echo -e "\t\tChanging permissions on ${FILE}"
+				echo -e "\tpermission: chaning permissions to 755 on ${FILE}"
 				ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "chmod 755 ${FILE}"
 			fi
 		else
-			echo -e "\tCopying ${FILE}"
+			echo -e "\tcopy: ${FILE}"
 			scp -i ${PEM_FILE} ${DB_SCRIPT_DIR}/${FILE} ec2-user@${HOST_IP}:./ 2<&1 > /dev/null
 	
 			if [ ${FILE} == "postgres_setup.sh" ]; then
-				echo -e "\t\tChanging permissions on ${FILE}"
+				echo -e "\tpermission: chaning permissions to 755 on ${FILE}"
 				ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "chmod 755 ${FILE}"
 			fi
 		fi
@@ -52,16 +53,16 @@ ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo usermod -a -G docker ec2-user" 2<&1
 RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo grep docker /etc/group" )
 echo -e "\t\tDocker group info: ${RESULT}"
 
-echo -e "\tdocker: Get docker info"
-RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo docker info | grep Server\ Version: 2<&1")
-echo -e "\t\tDocker server version: ${RESULT}"
-
 echo -e "\tsystemctl: Start docker service"
 ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo systemctl start docker"
 
 echo -e "\tsystemctl: Verify docker service"
 RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo systemctl status docker | grep Active | sed -e 's/^ *//g'")
 echo -e "\t\tDocker status: ${RESULT}"
+
+echo -e "\tdocker: Get docker info"
+RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo docker info | grep Server\ Version:")
+echo -e "\t\tDocker server version: ${RESULT}"
 
 echo -e "\tifconfig: Display instance IPs"
 RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo ifconfig eth0 | grep inet\  | sed -e 's/^ *//g'")
@@ -75,11 +76,11 @@ echo -e "\tdocker: Installing docker compose"
 ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` \
                                       | sudo tee /usr/local/bin/docker-compose > /dev/null" 2<&1 > /dev/null
 
-echo -e "\tdocker: Setup docker compose permissions"
+echo -e "\tpermission: Setup docker compose permissions"
 ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo chmod +x /usr/local/bin/docker-compose"
 
-echo -e "\tdocker: Setup docker compose link"
-RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo ls -al /usr/bin/docker-compose 2<&1")
+echo -e "\tlink: Setup docker compose link"
+RESULT=$(ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo ls -1 /usr/bin/docker-compose 2<&1")
 if [ "${RESULT}" != "/usr/bin/docker-compose" ]; then
 	ssh -i ${PEM_FILE} ec2-user@${HOST_IP} "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
 fi
@@ -115,5 +116,7 @@ while [[ ${RESULT} != *"${SUCCESS}"* ]]
 		RESULT=$(nc -z -v -w1 ${HOST_IP} ${HTTP_PORT} 2<&1)
 	done
 echo -e "\t\tPort 80 available"
+
+echo -e "\n\tTest with: http://${HOST_IP}/hello\n"
 
 echo -e "Script complete\n"
